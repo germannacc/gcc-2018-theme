41a0db5c2b7228dad210d5e0aad1115921309c8d
Matt Hummel <mattrhummel@users.noreply.github.com>
2019-06-07 12:05:18 -0400

    Updates
    Various updates / cleanup

--- template-parts/content-getstarted.php
+++ template-parts/content-getstarted.php
@@ -1,134 +1,70 @@
-<?php
-//Getting Started Section
-$get_started_heading = get_field('get_started_heading');
-
-//Apply Box
-$apply_box_heading = get_field('apply_box_heading');
-$apply_box_description = get_field('apply_box_description');
-$apply_box_image = get_field('apply_box_image');
-$apply_box_url = get_field('apply_box_url');
-//Register Box
-$register_box_heading = get_field('register_box_heading');
-$register_box_description = get_field('register_box_description');
-$register_box_image = get_field('register_box_image');
-$register_box_url = get_field('register_box_url');
-//Paying Box
-$paying_box_heading = get_field('paying_box_heading');
-$paying_box_description = get_field('paying_box_heading');
-$paying_box_image = get_field('paying_box_image');
-$paying_box_url = get_field('paying_box_url');
-
-?>
 <div id="getting-started" class="row expanded gutter-small expanded">
-
-<div class="getting-started-content">
-
-  <h2 class="text-center text-shadow"><?php the_field('get_started_heading'); ?></h2>
-
-  <div class="small-12 medium-6 large-4 columns" data-equalizer-watch>
-
-      <?php //apply box
-       if( !empty($apply_box_image) ): ?>
-
-  <div class="getting-started-block text-center">
-
-  <?php
-      // vars
-      $url = $apply_box_image['url'];
-      $alt = $apply_box_image['alt'];
-
-      // thumbnail
-      $size = 'large';
-      $thumb = $apply_box_image['sizes'][ $size ];
-      $width = $apply_box_image['sizes'][ $size . '-width' ];
-      $height = $apply_box_image['sizes'][ $size . '-height' ];
-      ?>
-
-    <img src="<?php echo $thumb; ?>" alt="<?php echo $alt;?>" width="<?php echo $width;?>" height="<?php echo $height;?>">
-
-    <a href="<?php the_field('apply_box_url');?>">
-    <div class="block-section">
-      <h3><?php the_field('apply_box_heading'); ?></h3>
-      <p><?php the_field('apply_box_description'); ?></p>
+  <div class="getting-started-content">
+    <h2 class="text-center text-shadow"><?php the_field('get_started_heading'); ?></h2>
+    <div class="small-12 medium-6 large-4 columns" data-equalizer-watch>
+      <div class="getting-started-block text-center">
+        <?php
+        // vars
+        $image1 = get_field('apply_box_image');
+        $url = $image1['url'];
+        $alt = $image1['alt'];
+        // thumbnail
+        $size = 'large';
+        $thumb = $image1['sizes'][ $size ];
+        $width = $image1['sizes'][ $size . '-width' ];
+        $height = $image1['sizes'][ $size . '-height' ];
+        ?>
+        <img src="<?php echo $url; ?>" alt="<?php echo $alt;?>" width="<?php echo $width;?>" height="<?php echo $height;?>">
+        <a href="<?php the_field('apply_box_url'); ?>">
+          <div class="block-section">
+            <h3><?php the_field('apply_box_heading'); ?></h3>
+            <p><?php the_field('apply_box_description'); ?></p>
+          </div>
+          
+        </a>
+      </div>
     </div>
-    </a>
-
-
+    <div class="small-12 medium-6 large-4 columns" data-equalizer-watch>
+      <div class="getting-started-block text-center">
+        <?php  // vars
+        $image2 = get_field('register_box_image');
+        $url = $image2['url'];
+        $alt = $image2['alt'];
+        // thumbnail
+        $size = 'large';
+        $thumb = $image2['sizes'][ $size ];
+        $width = $image2['sizes'][ $size . '-width' ];
+        $height = $image2['sizes'][ $size . '-height' ];
+        ?>
+        <img src="<?php echo $url; ?>" alt="<?php echo $alt;?>" width="<?php echo $width;?>" height="<?php echo $height;?>">
+        <a href="<?php the_field('register_box_url');?>">
+          <div class="block-section">
+            <h3><?php the_field('register_box_heading'); ?></h3>
+            <p><?php the_field('register_box_description'); ?></p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="small-12 medium-6 large-4 columns" data-equalizer-watch>
+      <div class="getting-started-block text-center">
+        <?php  // vars
+        $image3 = get_field('paying_box_image');
+        $url = $image3['url'];
+        $alt = $image3['alt'];
+        // thumbnail
+        $size = 'large';
+        $thumb = $image3['sizes'][ $size ];
+        $width = $image3['sizes'][ $size . '-width' ];
+        $height = $image3['sizes'][ $size . '-height' ];
+        ?>
+        <img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>">
+        <a href="<?php the_field('paying_box_url');?>">
+          <div class="block-section">
+            <h3><?php the_field('paying_box_heading'); ?></h3>
+            <p><?php the_field('paying_box_description'); ?></p>
+          </div>
+        </a>
+      </div>
+    </div>
   </div>
-
-    <?php endif; ?>
-
-</div>
-
-<div class="small-12 medium-6 large-4 columns" data-equalizer-watch>
-
-<?php //Register Box
- if( !empty($register_box_image) ):  ?>
-
-  <div class="getting-started-block text-center">
-
-<?php  // vars
-  $url = $register_box_image['url'];
-  $alt = $register_box_image['alt'];
-
-  // thumbnail
-  $size = 'large';
-  $thumb = $register_box_image['sizes'][ $size ];
-  $width = $register_box_image['sizes'][ $size . '-width' ];
-  $height = $register_box_image['sizes'][ $size . '-height' ];
-     ?>
-              <img src="<?php echo $thumb; ?>" alt="<?php echo $alt;?>" width="<?php echo $width;?>" height="<?php echo $height;?>">
-
-              <a href="<?php the_field('register_box_url');?>">
-              <div class="block-section">
-                <h3><?php the_field('register_box_heading'); ?></h3>
-                <p><?php the_field('register_box_description'); ?></p>
-              </div>
-              </a>
-
-
-            </div>
-
-<?php endif; ?>
-
-
-</div>
-
-<div class="small-12 medium-6 large-4 columns" data-equalizer-watch>
-
-  <?php
-  //Payment Box
-  if( !empty($paying_box_image) ):  ?>
-
-    <div class="getting-started-block text-center">
-
-  <?php  // vars
-    $url = $paying_box_image['url'];
-    $alt = $paying_box_image['alt'];
-
-    // thumbnail
-    $size = 'large';
-    $thumb = $paying_box_image['sizes'][ $size ];
-    $width = $paying_box_image['sizes'][ $size . '-width' ];
-    $height = $paying_box_image['sizes'][ $size . '-height' ];
-  ?>
-                <img src="<?php echo $thumb; ?>" alt="<?php echo $alt;?>" width="<?php echo $width;?>" height="<?php echo $height;?>">
-
-
-                <a href="<?php the_field('paying_box_url');?>">
-                <div class="block-section">
-                  <h3><?php the_field('paying_box_heading'); ?></h3>
-                  <p><?php the_field('paying_box_description'); ?></p>
-                </div>
-                </a>
-
-
-              </div>
-
-  <?php endif; ?>
-
-</div>
-
-</div>
-
 </div>
--- template-parts/content-weather-alert.php
+++ template-parts/content-weather-alert.php
@@ -1,49 +1,40 @@
 <?php
-  $closing_announcement_text = get_field('closing_announcement_text', 'option');
-  $weather_alert_heading = get_field('weather_alert_heading', 'option');
-  $sign_up_for_alerts_text = get_field('sign_up_for_alerts_text', 'option');
-  $alerts_url = get_field('alerts_url', 'option');
-
+$closing_announcement_text = get_field('closing_announcement_text', 'option');
+$weather_alert_heading = get_field('weather_alert_heading', 'option');
+$sign_up_for_alerts_text = get_field('sign_up_for_alerts_text', 'option');
+$alerts_url = get_field('alerts_url', 'option');
 //Column 1 Heading
 if( !empty($closing_announcement_text) ): ?>
-
 <div class="callout small closing" data-closable>
   <div class="row expanded">
-
-      <div class="medium-7 large-8 columns gutter-small">
-
-        <?php //Alert Text
-        if( !empty($weather_alert_heading) ): ?>
-        <p class="lead"><?php echo $weather_alert_heading; ?></p>
-        <?php endif; ?>
-
-        <p><?php echo $closing_announcement_text ?></p>
-
-      </div>
-
-      <div class="medium-4 large-3 columns gutter-small">
-
-        <?php //Alert Button
-        if( !empty($sign_up_for_alerts_text) ): ?>
-
-        <a href="<?php echo $alerts_url ?>" class="button hollow large expanded">
-          <span class="fa fa-arrow-right" aria-hidden="true"></span>
-          <?php echo $sign_up_for_alerts_text ?>
-        </a>
-
-        <?php endif; ?>
-
-      </div>
-
-      <div class="medium-1 large-2 columns">
-
-        <button class="close-button" aria-label="Dismiss alert" type="button" data-close>
-          <span aria-hidden="true">&times;</span>
-        </button>
-
-      </div>
-
-   </div>
-
+    <div class="medium-8 large-9 columns ">
+      <?php //Alert Text
+      if( !empty($weather_alert_heading) ): ?>
+      <p class="lead"><?php echo $weather_alert_heading; ?></p>
+      <?php endif; ?>
+      <style type="text/css">
+      
+      .closing p {
+      padding: 0;
+      margin: 0 0 15px 0;
+      }
+      </style>
+      <p><?php echo $closing_announcement_text ?></p>
+    </div>
+    <div class="medium-4 large-3 columns">
+      <?php //Alert Button
+      if( !empty($sign_up_for_alerts_text) ): ?>
+      <a href="<?php echo $alerts_url ?>" class="button hollow large expanded alert-button">
+        <span class="fa fa-arrow-right" aria-hidden="true"></span>
+        <?php echo $sign_up_for_alerts_text ?>
+      </a>
+      <?php endif; ?>
+    </div>
+    <div class="medium-1 large-2 columns">
+      <button class="close-button" aria-label="Dismiss alert" type="button" data-close>
+      <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  </div>
 </div>
 <?php endif; ?>
