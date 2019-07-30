1b270869dda6d1a6c072d9e10e67f032a624de42
Matt Hummel <marketing@users.noreply.github.com>
2019-07-30 15:50:35 -0400

    Update
    Removed unneccesary code

--- header.php
+++ header.php
@@ -1,125 +1,40 @@
 <?php
 /**
-<<<<<<< HEAD
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package gccwp-2018
- */
-?>
-<!DOCTYPE html>
-<html class="no-js" <?php language_attributes(); ?>>
-<head>
-
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="x-ua-compatible" content="ie=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-<link rel="profile" href="http://gmpg.org/xfn/11">
-
-<?php wp_head(); ?>
-
-<?php // ace tracking code for
-if ( is_page('108') || $post->post_parent == '108' ) {  ?>
-<script type="text/javascript">
-var _monsido = _monsido || [];
-_monsido.push(['_setDomainToken', '4tsVVD4d1Iw5kplXbM3eow']);
-_monsido.push(['_withStatistics', 'true']);
-</script>
-<script async src="//cdn.monsido.com/tool/javascripts/monsido.js"></script>
-<?php } ?>
-
-<script type="text/javascript">
-     var _monsido = _monsido || [];
-     _monsido.push(['_setDomainToken', '14l5XpM9a7fdx1hVoA-QRQ']);
-     _monsido.push(['_withStatistics', 'true']);
-</script>
-<script async src="//cdn.monsido.com/tool/javascripts/monsido.js"></script>
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-11698249-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-11698249-1');
-</script>
-
-<?php //google analytics tracking for ace
-if ( is_page('108') || $post->post_parent == '108' ) { //ACE google tracking ?>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-100518862-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-100518862-1');
-</script>
-<?php } ?>
-
-</head>
-
-<body <?php body_class(); ?>>
-
-<div id="skip">
-  <a href="#main" class="show-on-focus"><?php esc_html_e('skip to content', 'gcc-wp-2018') ?></a>
-</div>
-
-<?php
-get_template_part( 'template-parts/content', 'weather-alert' );
-?>
-<div class="off-canvas-wrapper no-js">
-  <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-    <?php main_menu(); //main sidebar navigation ?>
-    <div class="off-canvas-content" data-off-canvas-content>
-       <?php  //quicklinks
-        get_template_part( 'template-parts/content', 'quicklinks' );
-       ?>
-        <main>
-=======
-* The header for our theme
 *
-* This is the template that displays all of the <head> section and everything up until <div id="content">
-  *
-  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
-  *
-  * @package gccwp-2018
-  */
-  ?>
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+*
+* @package gccwp-2018
+*/
+?>
   <!DOCTYPE html>
   <html class="no-js" <?php language_attributes(); ?>>
     <head>
+     
       <meta charset="<?php bloginfo( 'charset' ); ?>">
       <meta http-equiv="x-ua-compatible" content="ie=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="profile" href="http://gmpg.org/xfn/11">
       <?php wp_head(); ?>
+      
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-T2KLMW');</script>
+      <!-- End Google Tag Manager -->
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2KLMW"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
+      
       <script type="text/javascript">
       var _monsido = _monsido || [];
       _monsido.push(['_setDomainToken', '14l5XpM9a7fdx1hVoA-QRQ']);
       _monsido.push(['_withStatistics', 'true']);
       </script>
       <script async src="//cdn.monsido.com/tool/javascripts/monsido.js"></script>
-      <?php //google analytics tracking for ace
-      if ( is_page('108') || $post->post_parent == '108' ) { //ACE google tracking ?>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-100518862-1"></script>
-      <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}s
-      gtag('js', new Date());
-      gtag('config', 'UA-100518862-1');
-      </script>
-      <?php } ?>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-11698249-1"></script>
-      <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-11698249-1');
-      
-      </script>
+
     </head>
     <body <?php body_class(); ?>>
       <div id="skip">
@@ -136,4 +51,3 @@ get_template_part( 'template-parts/content', 'weather-alert' );
             get_template_part( 'template-parts/content', 'quicklinks' );
             ?>
             <main>
->>>>>>> 2e005af4d960ad8060d6c95a9375ccffa7a06d70
